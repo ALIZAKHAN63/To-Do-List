69c310ff1aa6277045fe957e420bddb2db3b2321
ALIZAKHAN63 <alizakhan99163@gmail.com>
2024-08-26 12:20:49 +0500

    complete

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-function addTask(){
-    var taskInput  = document.getElementById('taskInput')
-    var output = taskInput.value
-
-    var taskList =document.getElementById('taskList')
-    taskList.innerHTML = taskList.innerHTML +"<li>" + output + "</li>";
+function addTask() {
+    var taskInput = document.getElementById('taskInput')
+    var output = taskInput.value;
+    if (output === "") {
+        alert("please enter task")
+        return;
+    }
+    var taskList = document.getElementById('taskList')
+    taskList.innerHTML = taskList.innerHTML + "<li>" + output + " <button onclick='completeTask(this)'>✔️</button></li>";
 
     taskInput.value = "";
+
 }
+
+function completeTask(button) {
+    var taskItem = button.parentElement;
+    button.remove();
+
+    var completedList = document.getElementById('CompleteTaskContainer');
+    completedList.innerHTML = completedList.innerHTML + "<li>" + taskItem.innerHTML + "</li>";
+
+    taskItem.remove();
+}
+
+
+
+
+
+
+
+
+
 
 
 
